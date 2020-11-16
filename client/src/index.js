42eaa43565c6af7d1b36180a53d2d1d13ecfa155
saranya <drssaranyasri@gmail.com>
2020-11-15 17:17:45 -0800

    test commented.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
  import Pictwo from './Pictwo/Pictwo';
-// import App from './App';
+// import App from './App'; //temproarily commented
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
